532ccca73ff3cd836ad0cf20d56db4edc6825dd4
shuo.sun <shuo.sun@chivox.com>
2018-06-21 15:45:22 +0800

    jquery animate.css

--- javaProjects/src/Person.java
+++ javaProjects/src/Person.java
@@ -3,6 +3,8 @@ import java.io.File;
 public class Person {
 
     public static void main(String[] args) {
-
+        String recordFilePath = "abcd/abcd.wav";
+        recordFilePath = recordFilePath.replaceAll("[a-fA-F0-9]+.wav", 1234+ ".wav");
+        System.out.println(recordFilePath);
     }
 }
